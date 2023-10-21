e0e5635271c42c5a583d927a15c0e4f49bc266f8
devinfoo <145243927+devinfoo@users.noreply.github.com>
2023-10-21 13:58:03 -0500

    Update tc_pt_ac_lc_combined.ino

--- Sensors/Combined Sensors/tc_pt_ac_lc_combined.ino
+++ Sensors/Combined Sensors/tc_pt_ac_lc_combined.ino
@@ -1,7 +1,3 @@
-
-/* this code is specifically for a Teensy conencted to 2 pressure transducers and an I2C multiplexer which has thermocouple amplifiers connected to it.
-*/
-
 /* 
 TIME MEASUREMENT VARABLES
 -------------------------
@@ -14,20 +10,20 @@ unsigned long time_tc;
 unsigned long time_pc;
 unsigned long time_lc;
 unsigned long time_ac;
-
 unsigned long lastUpdateTC;
 unsigned long lastUpdatePC;
 unsigned long lastUpdateLC;
 unsigned long lastUpdateAC;
 
+// update intervals in microseconds
 int updateIntervalTC = 1000000;
 int updateIntervalPC = 5000;
 int updateIntervalLC = 12500;
 int updateIntervalAC = 5000;
 
 /* 
-THERMOCOUPLE IMPORTS, VARIBLE SETUP, ETC
-----------------------------------------
+THERMOCOUPLE SET UP
+-------------------
 */
 
 // import libaries
@@ -40,39 +36,37 @@ THERMOCOUPLE IMPORTS, VARIBLE SETUP, ETC
 #define TCA9548A_ADDRESS 0x70
 #define MCP9600_ADDRESS 0x67
 
-// thermocouple amplifier setup
-#define num_MCP9600 8 //number of thermocouples for the multiplexer [INPUT VARIABLE]
-Adafruit_MCP9600 mcp; // needed for mcp commands 
-
-float tc_hot;
-
 // initialize multiplexer I2C ports
 void tcaselect(uint8_t i) {
   if (i > 7) return;
- 
   Wire.beginTransmission(TCA9548A_ADDRESS);
   Wire.write(1 << i);
   Wire.endTransmission();  
 }
+
+// thermocouple and tca setup
+int num_MCP9600 = 8; //number of thermocouples for the multiplexer
+float tc_values[8]; // list of measurements
+Adafruit_MCP9600 mcp; // needed for mcp commands 
 
 /* 
 PRESSURE TRANSDUCER VARIBLE SETUP
 ---------------------------------
 */
 
-// pin setup
-const int pt1_pin = A6; //select the analog input pin for pressure transducer 1
-const int pt2_pin = A7; //select the analog input pin for pressure transducer 2
+// define pins for each PT
+const int pt1_pin = A6;
+const int pt2_pin = A7;
 
-// 300 psi PT values
+// 300 psi PT ADC values
 const int pt1_analog_zero = 102.4; //analog reading of pressure transducer at 0psi
 const int pt1_analog_max = 921.6; //analog reading of pressure transducer at 100psi
 const int pt1_psi_max = 300; //max psi value of transducer being used
 
-// 500 psi PT values (NEEDS TO BE UPDATED)
-const int pt2_analog_zero = 102.4; //analog reading of pressure transducer at 0psi
-const int pt2_analog_max = 921.6; //analog reading of pressure transducer at 100psi
-const int pt2_psi_max = 300; //max psi value of transducer being used
+// 500 psi PT ADC values
+  const int pt2_analog_zero = 102.4; //analog reading of pressure transducer at 0psi
+  const int pt2_analog_max = 921.6; //analog reading of pressure transducer at 100psi
+const int pt2_psi_max = 500; //max psi value of transducer being used
 
 // temp reading variables
 float pt_analog_reading1 = 0; //variable to store the value coming from the pressure transducer
@@ -83,15 +77,15 @@ float pt_digital_reading2 = 0; //variable to store the value coming from the pre
 
 /* 
 ACCELEROMETER SETUP
----------------------------------
+-------------------
 */
 
-// custom libraries
+// import libraries
 #include "IMU.h"
 #include "LSM6DSL.h"
 #include "LIS3MDL.h"
 
-// variable setup
+// measurement variable setup
 byte buff[6];
 int accRaw[3];
 float accx, accy, accz;
@@ -102,29 +96,25 @@ LOAD CELL SET UP
 ------------------------------
 */
 
-// library
+// import library
 #include "HX711.h"
 
-// load cell 1
+// define data and clock pins for each loadcell
 const int LOADCELL_DOUT_PIN = 5;
 const int LOADCELL_SCK_PIN = 4;
-
-// load cell 2
 // const int LOADCELL_DOUT_PIN2 = 6;
 // const int LOADCELL_SCK_PIN2 = 7;
 
+// measurement set up
 float weight1;
 float weight2;
 HX711 scale, scale2;
 
-unsigned long loopStartTime; // Variable to store the start time of the loop
-
 /* 
 VOID SETUP FUNCTION
----------------------------------------
+-------------------
 */
 
-// initialize thermocouple amplifiers
 void setup(void) {
 
   /* 
@@ -190,6 +180,7 @@ void setup(void) {
   mcp.setAlertTemperature(1, 30);
   Serial.print("Alert #1 temperature set to ");
   Serial.println(mcp.getAlertTemperature(1));
+  Serial.println();
   mcp.configureAlert(1, true, true);  // alert 1 enabled, rising temp
 
   // not sure what mcp.enable() does
@@ -207,6 +198,7 @@ void setup(void) {
   Serial.println("IMU Detected...");
   enableIMU();
   Serial.println("IMU Enabled...");
+  Serial.println();
 
   /*
   LOAD CELL SET UP
@@ -214,7 +206,6 @@ void setup(void) {
   */
 
   Serial.begin(500000);
-
   Serial.println("Initializing the scale");
 
   scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
@@ -227,37 +218,37 @@ void setup(void) {
   // scale2.set_scale(2280.f); // Set the scale factor for conversion to kilograms
   // scale2.tare();            // Reset the scale to zero
 
-  Serial.println("After taring the scale:");
-
+  Serial.println("Finished taring the scale");
+  Serial.println();
   Serial.println(F("------------------------------"));
 }
 
 /* 
 MEASUREMENT LOOP
--------------------------------------------
+----------------
 */
+
 void loop(void) { 
   
-  if((micros() - lastUpdateTC) > updateIntervalTC){
-  lastUpdateTC = micros();
-
   /*
   THERMOCOUPLE READINGS
   ---------------------
   */
 
-  // start recording temp time
+  // check time since last update
+  if((micros() - lastUpdateTC) > updateIntervalTC){
+  lastUpdateTC = micros();
+
+  // start recording measurement time
   time_start = micros();
 
-  // read all sensors temps
+  // measure temperature from each thermocouple
   for (int i = 1; i <= num_MCP9600; ++i) {
     tcaselect(i - 1);
-    tc_hot = mcp.readThermocouple();
-    Serial.print("TC "); Serial.print(i); Serial.print(" "); Serial.print("Temperature (C):        "); Serial.println(tc_hot); // measure & print hot junction temp 
-  // Serial.print("Cold Temp (C):       "); Serial.println(mcp.readAmbient()); // cold junction temp
+    tc_values[i - 1] = mcp.readThermocouple();
   }
 
-  // record total time to measure temp
+  // end measurement time recording
   time_stop = micros();
   time_tc = time_stop - time_start;
   }
@@ -267,10 +258,11 @@ void loop(void) {
   ----------------------------
   */
 
+  // check time since last update
   if((micros() - lastUpdatePC) > updateIntervalPC){
   lastUpdatePC = micros();
 
-  // start recording pressure time
+  // start recording measurement time
   time_start = micros();
 
   // measure pressure from pt1
@@ -281,79 +273,89 @@ void loop(void) {
   pt_analog_reading2 = analogRead(pt2_pin); //reads value from input pin and assigns to variable
   pt_digital_reading2 = (pt_analog_reading2 - pt2_analog_zero)/(pt2_analog_max - pt2_analog_zero)*pt2_psi_max/.66; //conversion equation to convert analog reading to psi
 
-  // record total time to measure pressure
+  // end measurement time recording
   time_stop = micros();
   time_pc = time_stop - time_start;
   }
 
   /*
   ACCELEROMETER READINGS
-  ----------------------------
+  ----------------------
   */
 
+  // check time since last update
   if((micros() - lastUpdateAC) > updateIntervalAC){
   lastUpdateAC = micros();
 
+  // start recording measurement time
   time_start = micros();
 
+  // measure acceleration
   readACC(buff);
   accRaw[0] = (int16_t)(buff[0] | (buff[1] << 8)); 
   accRaw[1] = (int16_t)(buff[2] | (buff[3] << 8));
   accRaw[2] = (int16_t)(buff[4] | (buff[5] << 8));
-
   accx = accRaw[0]/accoffset;
   accy = accRaw[1]/accoffset;
   accz = accRaw[2]/accoffset;
 
+  // end measurement time recording
   time_stop = micros();
   time_ac = time_stop - time_start;
   }
 
   /*
   LOAD CELL READINGS
-  -------------------------------
+  ------------------
   */
 
-
+  // check time since last update
   if((micros() - lastUpdateLC) > updateIntervalLC){
   lastUpdateLC = micros();
   
-  loopStartTime = micros(); // Record the start time of the loop
+  // start recording measurement time
+  time_start = micros();
 
-  // Reading the weight from both load cells
+  // measure force from each load cell
   weight1 = scale.get_units(1);  // Get the weight in kilograms
-  weight2 = scale2.get_units(1); // Get the weight in kilograms
+  // weight2 = scale2.get_units(1); // Get the weight in kilograms
 
-  // Calculate and print the loop duration
-  unsigned long loopEndTime = micros(); // Record the end time of the loop
-  unsigned long time_lc = loopEndTime - loopStartTime;
+  // end measurement time recording
+  time_stop = micros();
+  time_lc = time_stop - time_start;
   }
 
   /*
-  SERIAL PRINTING READINGS
+  MEASUREMENT VALUE READINGS
+  --------------------------
   */
 
-  Serial.print("\nPT 1 Voltage  (mV):          "); Serial.println(pt_analog_reading1, 1); //prints calculated psi value
+  for (int i = 1; i <= num_MCP9600; ++i) {
+    Serial.print("TC "); Serial.print(i); Serial.print(" "); Serial.print("Temperature (C):        "); Serial.println(tc_values[i - 1]); // print hot junction temp 
+  }
+  Serial.println();
+  Serial.print("PT 1 Voltage  (mV):          "); Serial.println(pt_analog_reading1, 1); //prints calculated mV value
   Serial.print("PT 1 Pressure (psi):         "); Serial.println(pt_digital_reading1, 1); //prints calculated psi value
-  Serial.print("PT 2 Voltage  (mV):          "); Serial.println(pt_analog_reading2, 1); //prints calculated psi value
+  Serial.print("PT 2 Voltage  (mV):          "); Serial.println(pt_analog_reading2, 1); //prints calculated mV value
   Serial.print("PT 2 Pressure (psi):         "); Serial.println(pt_digital_reading2, 1); //prints calculated psi value
   Serial.println();
-  Serial.print("Acceleration (x, y, z) (g): "); Serial.print(accx); Serial.print(", "); Serial.print(accy); Serial.print(", "); Serial.print(accz); 
+  Serial.print("Acceleration in X (g):       "); Serial.println(accx);
+  Serial.print("Acceleration in Y (g):       "); Serial.println(accy);
+  Serial.print("Acceleration in Z (g):       "); Serial.println(accz);
   Serial.println();
-  Serial.print("LC 1 Force (kg):           ");
-  Serial.println(weight1, 1);
-  Serial.print("LC 2 Force (kg):           ");
-  Serial.println(weight2, 1);
+  Serial.print("LC 1 Force (kg):             "); Serial.println(weight1, 1);  // print load in kg 
+  Serial.print("LC 2 Force (kg):             ");   Serial.println(weight2, 1); //print load in kg
+  Serial.println();
 
   /*
   MEASUREMENT TIME READINGS 
   -------------------------
   */
 
-  Serial.print("\nTC Measurement Time (μs):     "); Serial.println(time_tc);
-  Serial.print("PC Measurement Time (μs):       "); Serial.println(time_pc);
-  Serial.print("AC Measurement Time (μs):       "); Serial.println(time_ac);
-  Serial.print("LC Measurement Time (μs):       "); Serial.println(time_lc);
-
+  Serial.print("TC Measurement Time (μs):     "); Serial.println(time_tc);
+  Serial.print("PT Measurement Time (μs):       "); Serial.println(time_pc);
+  Serial.print("AC Measurement Time (μs):      "); Serial.println(time_ac);
+  Serial.print("LC Measurement Time (μs):    "); Serial.println(time_lc);
   Serial.println(F("----------------------------------"));
+  delay(5);
 }
